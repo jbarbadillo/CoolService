c8ce1e2520aec7b3e2280877a4ab48c7ea3e5e4a
jbarbadillo <javier.barbadillo@gmail.com>
2018-03-17 15:53:31 +0100

    Add logger

--- app/src/main/java/com/javirock/coolservice/CoolService.java
+++ app/src/main/java/com/javirock/coolservice/CoolService.java
@@ -4,6 +4,9 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.Logger;
+
 public class CoolService extends Service {
     public CoolService() {
     }
@@ -12,5 +15,38 @@ public class CoolService extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Logger.addLogAdapter(new AndroidLogAdapter());
+
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        /*if (intent.getAction().equals(Constants.ACTION.STARTFOREGROUND_ACTION)) {
+            showNotification();
+            Toast.makeText(this, "Service Started!", Toast.LENGTH_SHORT).show();
+
+        } else if (intent.getAction().equals(Constants.ACTION.PREV_ACTION)) {
+            Log.i(LOG_TAG, "Clicked Previous");
+
+            Toast.makeText(this, "Clicked Previous!", Toast.LENGTH_SHORT)
+                    .show();
+        } else if (intent.getAction().equals(Constants.ACTION.PLAY_ACTION)) {
+            Log.i(LOG_TAG, "Clicked Play");
+
+            Toast.makeText(this, "Clicked Play!", Toast.LENGTH_SHORT).show();
+        } else if (intent.getAction().equals(Constants.ACTION.NEXT_ACTION)) {
+            Log.i(LOG_TAG, "Clicked Next");
+
+            Toast.makeText(this, "Clicked Next!", Toast.LENGTH_SHORT).show();
+        } else if (intent.getAction().equals(
+                Constants.ACTION.STOPFOREGROUND_ACTION)) {
+            Log.i(LOG_TAG, "Received Stop Foreground Intent");
+            stopForeground(true);
+            stopSelf();
+        }*/
+        return START_STICKY;
     }
 }
